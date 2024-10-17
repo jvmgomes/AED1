64ad46b75be8a0ea0cf467b4ad4c7c870b67ec16
jvmgomes <joao@mgomes.net>
2024-10-17 01:51:23 -0300

    Create fgets.c

---
+++ fgets.c
@@ -0,0 +1,21 @@
+//
+// Created by jvmgo on 10/10/2024.
+//
+#include <stdio.h>
+
+int main(void) {
+    FILE *arquivo;
+    char *resultado, nome[100];
+    arquivo = fopen("arquivo_teste", "r");
+    if(arquivo == NULL) {
+        printf("Erro na leitura do arquivo");
+        return 1;
+    }
+    while(!feof(arquivo)) {
+        resultado = fgets(nome, 100, arquivo);
+        if(resultado) printf("%s", nome);
+    }
+    fclose(arquivo);
+    
+    return 0;
+}
