a0510705d92fcd5b9251e8157179d0fa89ae9c62
jvmgomes <joao@mgomes.net>
2024-11-06 21:24:41 -0300

    Create 1020.c

---
+++ Exercicios/1020.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int diasTotais, dias, meses, anos;
+
+    scanf("%d", &diasTotais);
+
+    anos = diasTotais / 365;
+    meses = diasTotais % 365 /30;
+    dias =  diasTotais % 365 % 30;
+
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses);
+    printf("%d dia(s)\n", dias);
+
+
+    return 0;
+}
