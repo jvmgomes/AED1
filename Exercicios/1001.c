fa8f9af1a09276811cc5042d43c204e81a721a41
jvmgomes <joao@mgomes.net>
2024-11-18 23:29:10 -0300

    Create 1001.c

---
+++ Exercicios/1001.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, soma;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    soma = A + B;
+    
+    printf("X = %d\n", soma);
+ 
+    return 0;
+}
