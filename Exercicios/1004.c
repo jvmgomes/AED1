acbca1fd21b4e7207a09ff11744db5daf78be935
jvmgomes <joao@mgomes.net>
2024-11-18 23:29:38 -0300

    Create 1004.c

---
+++ Exercicios/1004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A, B, produto;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    produto = A * B;
+    
+    printf("PROD = %d\n", produto);
+    
+    return 0;
+}
