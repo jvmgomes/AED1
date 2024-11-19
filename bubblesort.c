0c997c0e416aaaf16b22a8eedc91dfd90a4d15f4
jvmgomes <joao@mgomes.net>
2024-11-19 16:31:25 -0300

    bubble

---
+++ bubblesort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int bubblesort(int vet[], int n){
+    int temp;
+    for(int i=0; i<n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            if(vet[j] > vet[j+1]){
+                temp = vet[j];
+                vet[j] = vet[j+1];
+                vet[j+1] = temp;
+            }
+        }
+    }
+    
+    for(int i=0; i < n; i++)
+        printf("%d ", vet[i]);
+}
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    int vet[n];
+    for(int i=0; i<n; i++){
+        scanf("%d", &vet[i]);
+    }
+
+    bubblesort(vet, n);
+    return 0;
+}
