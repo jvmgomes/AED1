5a62f62c601c02b7145c1785ae3cfcb4564eabfa
jvmgomes <joao@mgomes.net>
2025-01-28 11:31:11 -0300

    Create 2006.c

---
+++ Exercicios/2006.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void){
+    int t, numeros[1000], total = 0;
+
+    scanf("%d", &t);
+
+    
+    for(int i=0; i<=4; i++){
+        scanf("%d", &numeros[i]);
+    }
+    for(int i=0; i<=4; i++){
+        if(numeros[i]==t){
+            total++;
+        }
+    }
+    printf("%d\n", total);
+
+    return 0;
+}
