6022573e464452bf3c1e1d6653decac83e5b2cc4
jvmgomes <joao@mgomes.net>
2025-01-28 11:34:49 -0300

    Create 1038.c

---
+++ Exercicios/1038.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void){
+    int codigo, quant;
+    double total;
+    scanf("%d %d", &codigo, &quant);
+
+    if(codigo==1){
+        total += 4.00;
+    }
+    else if(codigo==2){
+        total += 4.50;
+    }
+    else if(codigo==3){
+        total += 5.00;
+    }
+    else if(codigo==4){
+        total += 2.00;
+    }
+    else if(codigo==5){
+        total += 1.50;
+    }
+    total *= quant;
+    printf("Total: R$ %.2lf\n", total);
+
+    return 0;
+}
