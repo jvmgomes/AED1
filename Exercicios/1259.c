3b2872329ff34e3f9e24ea4529cd8d7aa327debc
jvmgomes <joao@mgomes.net>
2025-01-28 11:32:35 -0300

    Create 1259.c

---
+++ Exercicios/1259.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+int comp(const void *a, const void *b)
+{
+    int A = *(int *)a, B = *(int *)b;
+
+    if (A % 2 == 0)
+        if (B % 2 == 0)
+            return A - B;
+        else
+            return -1;
+    else if (B % 2 == 0)
+        return 1;
+    else
+        return B - A;
+}
+
+int main(void){
+    int n, numeros[1000];
+    scanf("%d", &n);
+
+    for(int i=0; i<n; ++i){
+        scanf("%d", &numeros[i]);
+
+    }
+    qsort(numeros, n, sizeof(int), comp);
+
+    for(int i=0; i<n; ++i){
+        printf("%d\n", numeros[i]);
+    }
+
+    return 0;
+}
