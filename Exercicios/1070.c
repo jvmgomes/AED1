2a522bc8de33b561abe2ca3cc8f8d462d1bda487
jvmgomes <joao@mgomes.net>
2025-01-28 11:33:28 -0300

    Create 1070.c

---
+++ Exercicios/1070.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int x;
+    scanf("%d", &x);
+    for(int i=x; i<x+12; i++)
+        if(i%2!=0){
+            printf("%d\n", i);
+        }
+    return 0;
+}
